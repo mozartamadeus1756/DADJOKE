443e63b9b94dcfa1f87733a0ee3b460193d56357
kine <kine.sandvik07@gmail.com>
2025-01-15 11:15:12 +0100

    add .gitignore to ignore enviroment files

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("click", function(event){
     })
     .then(response => {
         if(!response.ok) {
-            throw new Error('eeroor');
+            throw new Error('error');
         }
         return response.json();
     })
