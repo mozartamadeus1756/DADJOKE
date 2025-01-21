bfd4b48f9743812eba549d2a8b5133973e37f052
kine <kine.sandvik07@gmail.com>
2025-01-21 13:04:30 +0100

    i did it!

--- server.js
+++ server.js
@@ -5,31 +5,31 @@ const pool = mariadb.createPool({
     host: 'localhost', // process.env.DB_HOST,
     user: 'kine', //process.env.DB_USER,
     password: 'kulekine1234', //process.env.DB_PASSWORD,
-    database: 'dada_jokes', //process.env.DB_NAME
+    database: 'dada_joke', //process.env.DB_NAME
     connectionLimit: 5
 });
 
 
-// async function asyncFunction() {
-//     let conn;
-//     try {
-//         const joke = 'ejkeene';
-//         const date = '2009-12-07';
-//       conn = await pool.getConnection();
-//       const rows = await conn.query("SELECT * FROM jokes");
-//       console.log(rows); //[ {val: 1}, meta: ... ]
-//       const res = await conn.query(`INSERT INTO jokes values (${joke}, ${date})`);
-//       console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+async function asyncFunction() {
+    let conn;
+    try {
+        const joke = 'ejkeene';
+        const date = '2009-12-07';
+      conn = await pool.getConnection();
+      const rows = await conn.query("SELECT * FROM jokes");
+      console.log(rows); //[ {val: 1}, meta: ... ]
+      const res = await conn.query("INSERT INTO jokes (joke, date) VALUES (?, ?)", [joke, date]); // (${joke}, ${date})
+      console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
   
-//     } catch (err) {
-//       throw err;
-//     } finally {
-//       if (conn) await conn.end();
-//     }
-//   }
-//   asyncFunction().then(() => {
-//     pool.end();
-//   })
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) await conn.end();
+    }
+  }
+  asyncFunction().then(() => {
+    pool.end();
+  })
 
 
 
