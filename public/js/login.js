b0e6f8236332eeea02bb0c387674ca5e085d7113
kine <kine.sandvik07@gmail.com>
2025-03-04 10:36:44 +0100

    added usernames to register and login page

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('.login-form');
     loginForm.addEventListener('submit', async (e) => {
         e.preventDefault();
-        
+        const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const response = await fetch('http://localhost:5501/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password })
+                body: JSON.stringify({ username, email, password })
             });
             const data = await response.json();
             if (data.success) {
--- public/js/register.js
+++ public/js/register.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
-
+        const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const confirmPassword = document.getElementById('confirm-password').value;
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password })
+                body: JSON.stringify({ username, email, password })
             });
 
             const data = await response.json();
